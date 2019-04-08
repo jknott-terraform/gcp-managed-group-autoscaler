074324b284b1ed0a245186ae808368dd23833922
jknott-terraform <49401490+jknott-terraform@users.noreply.github.com>
2019-04-08 13:13:48 -0500

    Bare Bones till I get the compute advanced settings moved over

---
+++ main.tf
@@ -0,0 +1,63 @@
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "f1-micro"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "debian-cloud/debian-9"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  instance_template  = "${google_compute_instance_template.default.self_link}"
+  base_instance_name = "instance-group-manager"
+  zone               = "us-central1-a"
+  target_size        = "3"
+}
+
+resource "google_compute_autoscaler" "webservers" {
+  name   = "my-autoscaler"
+  zone   = "us-central1-a"
+  target = "${google_compute_instance_group_manager.instance_group_manager.self_link}"
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 3
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5 
+    }
+  }
+}
+
